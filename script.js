ad7304b5e5d56fdd96917e5873fb6998feb5e70c
Aidan Lalonde-Novales <aidan.lalonde-novales@ocsbstudent.ca>
2020-12-15 15:48:24 +0000

    i fixed the stuff i think

--- script.js
+++ script.js
@@ -3,18 +3,18 @@
 document.getElementById('submitbutton').addEventListener('click', areaCalculation)
 
 function areaCalculation () {
- const alength = document.getElementById('alength').value
- // Puts the length of a into a variable.
- const blength = document.getElementById('blength').value
- // Puts the length of b into a variable.
- const height = document.getElementById('height').value
- // Puts the height into a variable.
- const ab1 = parseInt(alength) + parseInt(blength)
- // Adds a and b.
- const ab2 = ab1 / 2
- // Divides the sum of a and b by 2.
- const areatotal = ab2 * height
- // Multiplies the last calculation by height.
- document.getElementById('area').innerHTML = areatotal
- // Displays final area.
+  const alength = document.getElementById('alength').value
+  // Puts the length of a into a variable.
+  const blength = document.getElementById('blength').value
+  // Puts the length of b into a variable.
+  const height = document.getElementById('height').value
+  // Puts the height into a variable.
+  const ab1 = parseInt(alength) + parseInt(blength)
+  // Adds a and b.
+  const ab2 = ab1 / 2
+  // Divides the sum of a and b by 2.
+  const areatotal = ab2 * height
+  // Multiplies the last calculation by height.
+  document.getElementById('area').innerHTML = areatotal
+  // Displays final area.
 }
